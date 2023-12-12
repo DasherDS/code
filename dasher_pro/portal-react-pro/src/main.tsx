3d466ad3d859ad819e31123a18d4f01f97a6c31d
dasher <dxdasher@163.com>
2023-12-12 22:13:35 +0800

    feat: 移动端pc之前切换

--- dasher_pro/portal-react-pro/src/main.tsx
+++ dasher_pro/portal-react-pro/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@arco-themes/react-colorblue/css/arco.css";
+import "@arco-design/mobile-react/dist/style.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ dasher_pro/portal-react-pro/src/mobile/startup.ts
@@ -0,0 +1,8 @@
+
+import setRootPixel from "@arco-design/mobile-react/tools/flexible";
+
+const startup = () => {
+  setRootPixel(37.5);
+};
+
+export default startup;
